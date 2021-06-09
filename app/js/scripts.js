324e7d04c58a0a3003d28eabb1d83eee97a790ee
Daniel Deverell <daniel.deverell@gmail.com>
2021-06-08 21:19:49 -0400

    added css and basic js

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,1 +1,13 @@
-// scripts go here
+var tabs = document.querySelectorAll("nav a");
+
+tabs.forEach((tab) => tab.addEventListener("click", makeActive));
+
+function makeActive(event) {
+  makeInactive();
+  event.target.classList.add("active");
+  event.preventDefault();
+}
+
+function makeInactive() {
+  tabs.forEach((tab) => tab.classList.remove("active"));
+}
