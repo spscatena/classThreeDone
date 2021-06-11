792b89cc38171c4b179b5bb11203721675f01b36
Sabrina Scatena <sabrina.scatena@gmail.com>
2021-06-10 21:17:27 -0400

    added content to page

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,13 +1,34 @@
-var tabs = document.querySelectorAll("nav a");
-
-tabs.forEach((tab) => tab.addEventListener("click", makeActive));
+var tabs = document.querySelectorAll('nav a');
+contentPara = document.querySelector('.content');
 
 function makeActive(event) {
+  if (!event.target.matches('a')) return;
   makeInactive();
-  event.target.classList.add("active");
-  event.preventDefault();
+  event.target.classList.add('active');
 }
 
 function makeInactive() {
-  tabs.forEach((tab) => tab.classList.remove("active"));
+  tabs.forEach((tab) => tab.classList.remove('active'));
 }
+
+function setContentAccordingToHash() {
+  const type = window.location.hash.substring(1);
+  contentPara.innerHTML = data[type];
+}
+
+function initializePage() {
+  if (!window.location.hash) {
+    window.location.hash = 'cuisines';
+    document.querySelector('[href="#cuisines"]').classList.add('active');
+  } else {
+    document
+      .querySelector(`[href="${window.location.hash}]"`)
+      .classList.add('active');
+  }
+  setContentAccordingToHash();
+}
+
+document.addEventListener('click', makeActive);
+window.addEventListener('hashchange', setContentAccordingToHash);
+
+initializePage();
